378b4c98bb82a004100af9002fd985663bf38176
Aron Kunze <aron.kunze@gmail.com>
2019-01-30 13:47:41 +0100

    cleanup

--- lib/fog/openstack/auth/token.rb
+++ lib/fog/openstack/auth/token.rb
@@ -14,79 +14,11 @@ module Fog
         class URLError < RuntimeError; end
 
         def self.build(auth, options)
-          if auth[:openstack_identity_api_version] =~ /(v)*2(\.0)*/i ||
-             auth[:openstack_tenant_id] || auth[:openstack_tenant]
-            token = Fog::OpenStack::Auth::Token::V2.new(auth, options)
-          else
-            token = Fog::OpenStack::Auth::Token::V3.new(auth, options)
-          end
           if auth[:openstack_service_provider]
-            service_provider = token.data["token"]["service_providers"].select{ |x| 
-              x["id"] == auth[:openstack_service_provider]
-            }.first
-            auth[:openstack_domain_id] = "default"
-            if service_provider.present?
-              c = token.credentials.clone
-              p c
-              c["auth"]["scope"] = { service_provider: { id: auth[:openstack_service_provider]}}
-              saml_assertion = Excon.post(
-                "#{auth[:openstack_auth_url]}#{auth.fetch(:federation_url, '/v3/auth/OS-FEDERATION/saml2/ecp')}", 
-                body: Fog::JSON.encode(c),
-                idempotent: true, 
-                retry_limit: 5, 
-                retry_interval: 1
-              )
-              p saml_assertion
-              auth_cookie_response = Excon.post(service_provider["sp_url"], 
-                                       body: saml_assertion.body,
-                                       headers: { "Content-Type": "application/vnd.paos+xml" },
-                                       idempotent: true, 
-                                       retry_limit: 5, 
-                                       retry_interval: 1
-                                      )
-              auth_cookie = auth_cookie_response.headers["Set-Cookie"]
-              p auth_cookie
-
-
-              retries = 5
-              while retries > 0 do
-                unscoped_token_response = Excon.get(service_provider["auth_url"], 
-                                           headers: {
-                                                      "Content-Type": "application/vnd.paos+xml", 
-                                                      "Cookie": auth_cookie 
-                                                    },
-                                           idempotent: true, 
-                                           retry_limit: 5, 
-                                           retry_interval: 1
-                                          )
-
-                p ""
-                p unscoped_token_response
-                p ""
-                if unscoped_token_response.status < 400
-                  unscoped_token = unscoped_token_response.headers["X-Subject-Token"]
-                  break
-                end
-                retries -= 1
-              end
-              p ""
-              p unscoped_token
-              p ""
-              u = URI(service_provider["sp_url"])
-              p u
-
-              scoped_auth = { 
-                openstack_auth_url: "#{u.scheme}://#{u.host}:#{u.port}",
-                openstack_auth_token: unscoped_token
-              }.merge(
-                auth.slice(:openstack_project_name, :openstack_domain_id)
-              )
-              token = Fog::OpenStack::Auth::Token::V3.new(scoped_auth, options)
-              p ""
-              p "scoped_token: #{token}"
-            end
+            get_token_with_federated_identity(auth, options)
+          else
+            get_token_with_version(auth, options)
           end
-          token
         end
 
         def initialize(auth, options)
@@ -102,6 +34,90 @@ module Fog
         def get
           set(authenticate(@creds, {})) if expired?
           @token
+        end
+
+        def get_token_with_version(auth, options)
+          if auth[:openstack_identity_api_version] =~ /(v)*2(\.0)*/i ||
+             auth[:openstack_tenant_id] || auth[:openstack_tenant]
+            Fog::OpenStack::Auth::Token::V2.new(auth, options)
+          else
+            Fog::OpenStack::Auth::Token::V3.new(auth, options)
+          end
+        end
+
+        def get_token_with_federated_identity(auth, options)
+          default_auth = auth.merge({ openstack_domain_id: "default"})
+          token = get_token_with_version(default_auth)
+
+          service_provider = token.data["token"]["service_providers"].select { |x|
+            x["id"] == auth[:openstack_service_provider]
+          }.first
+
+          if service_provider.present?
+            credentials = token.credentials.clone
+            credentials["auth"]["scope"] = { service_provider: { id: auth[:openstack_service_provider]}}
+
+            saml_assertion = get_saml_assertion(default_auth, credentials)
+            auth_cookie = get_auth_cookie(service_provider, saml_assertion)
+            unscoped_token = get_unscoped_token(service_provider, auth_cookie)
+            scoped_token = get_scoped_token(auth, unscoped_token, options)
+            scoped_token
+          end
+        end
+
+        def get_saml_assertion(auth, credentials)
+          Excon.post(
+            "#{auth[:openstack_auth_url]}#{auth.fetch(:federation_url, '/v3/auth/OS-FEDERATION/saml2/ecp')}",
+            body: Fog::JSON.encode(credentials),
+            idempotent: true,
+            retry_limit: 5,
+            retry_interval: 1
+          )
+        end
+
+        def get_auth_cookie(service_provider, saml_assertion)
+          auth_cookie_response = Excon.post(
+            service_provider["sp_url"],
+            body: saml_assertion.body,
+            headers: { "Content-Type": "application/vnd.paos+xml" },
+            idempotent: true,
+            retry_limit: 5,
+            retry_interval: 1
+          )
+          auth_cookie_response.headers["Set-Cookie"]
+        end
+
+        def get_unscoped_token(service_provider, auth_cookie, retries = 5)
+          while retries > 0
+            unscoped_token_response = Excon.get(
+              service_provider["auth_url"],
+              headers: {
+                "Content-Type": "application/vnd.paos+xml",
+                "Cookie": auth_cookie 
+              },
+             idempotent: true,
+             retry_limit: 5,
+             retry_interval: 1
+            )
+            if unscoped_token_response.status < 400
+              unscoped_token = unscoped_token_response.headers["X-Subject-Token"]
+              break
+            end
+            retries -= 1
+          end
+          unscoped_token
+        end
+
+        def get_scoped_token(auth, service_provider, unscoped_token, options)
+          u = URI(service_provider["sp_url"])
+
+          scoped_auth = {
+            openstack_auth_url: "#{u.scheme}://#{u.host}:#{u.port}",
+            openstack_auth_token: unscoped_token
+          }.merge(
+            auth.slice(:openstack_project_name, :openstack_domain_id)
+          )
+          get_token_with_version(scoped_auth, options)
         end
 
         private
